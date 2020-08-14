b745c7a0a96f00613ec5f9c2502ec721af04b881
IngridCBarbosa <ingridcarolinelima@hotmail.com>
2020-08-14 17:56:40 -0300

    add calculator structure

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Calculator.css';
+
+function Calculator() {
+    return(
+        <div className="calculator">
+            
+        </div>
+    );
+}
+
+export default Calculator;
