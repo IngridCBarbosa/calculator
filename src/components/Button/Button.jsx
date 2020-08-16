b23293600cc950063aee90d70ffa18ad85acd76e
IngridCBarbosa <ingridcarolinelima@hotmail.com>
2020-08-16 14:39:10 -0300

    adjustes in Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 
 import './styles.css';
 
-function Button (props) {
+function Button(props) {
     return (
-    <button className="button">{props.label}</button>
+        <button
+            className={
+                `
+                button 
+                ${props.operation ? 'operation' : ''}
+                ${props.double ? 'double':''}
+                ${props.triple ? 'triple':''} 
+                
+                `
+            }
+            onClick={e => props.click && props.click(props.label)}
+        >
+            {props.label}
+        </button>
     );
 }
 
