16dd2dd9ccc2987903207613be91b79552174157
IngridCBarbosa <ingridcarolinelima@hotmail.com>
2020-08-17 14:01:12 -0300

    add part logic part one

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState} from 'react';
 
 import './Styles.css';
 
@@ -8,39 +8,60 @@ import Display from '../components/Display/Display';
 
 function Calculator() {
 
+    const [displayValue, setDisplayValue] = useState('0');
+    const [values, setValue] = useState([0, 0]);
+    const [operations, setOperations] = useState({});
+    const [current, setCurrent] = useState(0);
+
     function clearMemory() {
-        console.log('clear');
+        setDisplayValue('0');
     }
 
     function setOperation(operation) {
-        console.log(operation);
+        //console.log(operation);
     }
 
     function addDigit(digit) {
-        console.log(digit);
+
+        if (digit === '.' && displayValue.includes('.')) {
+            return;
+        }
+        const clearDisplay = displayValue === '0';
+        const currentValue = clearDisplay ? '' : displayValue;
+
+        setDisplayValue(currentValue + digit);
+
+        if(digit !== '.'){
+            
+            const value = [...values];
+            value[current] =  parseInt(displayValue);
+            setValue(value);
+            console.log(value);
+        }
+
     }
 
-    return(
-        
+    return (
+
         <div className="calculator">
-            <Display value={100}/>
-            <Button label="AC" click={() => clearMemory()} triple/>
-            <Button label="/" click={setOperation} operation/>
-            <Button label="7" click={addDigit}/>
-            <Button label="8" click={addDigit}/>
-            <Button label="9" click={addDigit}/>
-            <Button label="*" click={setOperation} operation/>
-            <Button label="4" click={addDigit}/>
-            <Button label="5" click={addDigit}/>
-            <Button label="6" click={addDigit}/>
-            <Button label="-" click={setOperation} operation/>
-            <Button label="1" click={addDigit}/>
-            <Button label="2" click={addDigit}/>
-            <Button label="3" click={addDigit}/>
-            <Button label="+" click={setOperation} operation/>
-            <Button label="0" click={addDigit} double/>
-            <Button label="." click={addDigit}/>
-            <Button label="=" click={setOperation} operation/>           
+            <Display value={displayValue} />
+            <Button label="AC" click={clearMemory} triple />
+            <Button label="/" click={setOperation} operation />
+            <Button label="7" click={addDigit} />
+            <Button label="8" click={addDigit} />
+            <Button label="9" click={addDigit} />
+            <Button label="*" click={setOperation} operation />
+            <Button label="4" click={addDigit} />
+            <Button label="5" click={addDigit} />
+            <Button label="6" click={addDigit} />
+            <Button label="-" click={setOperation} operation />
+            <Button label="1" click={addDigit} />
+            <Button label="2" click={addDigit} />
+            <Button label="3" click={addDigit} />
+            <Button label="+" click={setOperation} operation />
+            <Button label="0" click={addDigit} double />
+            <Button label="." click={addDigit} />
+            <Button label="=" click={setOperation} operation />
         </div>
 
     );
